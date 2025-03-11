c038cd6416022ce8ec8e8f6596793b7644f38847
Abdou <sall.abdou34@gmail.com>
2025-03-11 06:42:14 +0100

    searchbar

--- src/components/EmployeeTables.jsx
+++ src/components/EmployeeTables.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import DataTable from "react-data-table-component";
 
 const EmployeeTable = () => {
   const employees = useSelector((state) => state.employees);
+  const [search, setSearch] = useState("");
+
+  // Filtrer les employés en fonction de la recherche
+  const filteredEmployees = employees.filter((employee) =>
+    Object.values(employee).some((value) =>
+      value.toString().toLowerCase().includes(search.toLowerCase())
+    )
+  );
 
   // Définition des colonnes
   const columns = [
@@ -19,16 +28,20 @@ const EmployeeTable = () => {
 
   return (
     <div>
-
       <h2>Employee List</h2>
 
+      {/* Barre de recherche */}
+      <input
+        type="text"
+        placeholder="Rechercher un employé..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="search-input"
+      />
 
-        {/* //ajouter placeholder de search  */}
-
-      
       <DataTable
         columns={columns}
-        data={employees}
+        data={filteredEmployees}
         pagination
         highlightOnHover
       />
